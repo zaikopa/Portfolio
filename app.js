c72cd4515dda944035fdc46b4535ed713cdba035
zaikopa <lucianocabral2008@gmail.com>
2025-06-03 22:31:54 -0300

    se actualizo

---
+++ app.js
@@ -0,0 +1,60 @@
+function cargarAnimaciones() {
+    $(function() {
+        $('.chart1').easyPieChart({
+            size: 160,
+            barColor: "#ff5b00",
+            scaleLength: 0,
+            lineWidth: 15, // Corregido de lineWidht a lineWidth
+            trackColor: "#525151", // Corregido de tackcolor a trackColor
+            lineCap: "circle",
+            animate: 2000,
+        });
+        $('.chart2').easyPieChart({
+            size: 160,
+            barColor: "#ff5b00",
+            scaleLength: 0,
+            lineWidth: 15, // Corregido de lineWidht a lineWidth
+            trackColor: "#525151", // Corregido de tackcolor a trackColor
+            lineCap: "circle",
+            animate: 2000,
+        });
+        $('.chart3').easyPieChart({
+            size: 160,
+            barColor: "#ff5b00",
+            scaleLength: 0,
+            lineWidth: 15, // Corregido de lineWidht a lineWidth
+            trackColor: "#525151", // Corregido de tackcolor a trackColor
+            lineCap: "circle",
+            animate: 2000,
+        });
+        $('.chart4').easyPieChart({
+            size: 160,
+            barColor: "#ff5b00",
+            scaleLength: 0,
+            lineWidth: 15, // Corregido de lineWidht a lineWidth
+            trackColor: "#525151", // Corregido de tackcolor a trackColor
+            lineCap: "circle",
+            animate: 2000,
+        });
+        $('.chart5').easyPieChart({
+            size: 160,
+            barColor: "#ff5b00",
+            scaleLength: 0,
+            lineWidth: 15, // Corregido de lineWidht a lineWidth
+            trackColor: "#525151", // Corregido de tackcolor a trackColor
+            lineCap: "circle",
+            animate: 2000,
+        });
+        $('.chart6').easyPieChart({
+            size: 160,
+            barColor: "#ff5b00",
+            scaleLength: 0,
+            lineWidth: 15, // Corregido de lineWidht a lineWidth
+            trackColor: "#525151", // Corregido de tackcolor a trackColor
+            lineCap: "circle",
+            animate: 2000,
+        });
+    })
+}
+
+cargarAnimaciones();
